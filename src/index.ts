82bf0aa8c11728c6cd4a2b0abb24dd7ab94b6970
Johannes Oster <johannes.oster@outlook.at>
2021-04-16 22:38:25 +0200

    Fix port binding

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import server from 'infrastructure/server';
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

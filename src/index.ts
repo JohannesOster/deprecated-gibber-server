5711bcfd8c9e9d85e834ff6b649b80e90fff8fc2
Johannes Oster <johannes.oster@outlook.at>
2021-04-29 21:26:36 +0200

    Add pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import server from 'infrastructure/server';
+import pino from 'pino';
+
+const logger = pino();
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  logger.info(`Listening on port ${port}`);
 });

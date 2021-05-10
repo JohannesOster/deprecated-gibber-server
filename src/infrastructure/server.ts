e4dd56890309788e116f38bdc4b073fee46c4489
Johannes Oster <johannes.oster@outlook.at>
2021-05-10 14:30:59 +0200

    Add express

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,7 +1,13 @@
+import express from 'express';
 import {createServer as createHTTPServer} from 'http';
 import {createSocketServer} from './createSocketServer';
 
-const server = createHTTPServer();
+const app = express();
+const server = createHTTPServer(app);
 createSocketServer(server);
+
+app.get('/', (req, res) => {
+  res.json('Hello world!');
+});
 
 export default server;
